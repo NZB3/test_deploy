0e593a20a5b4fc258714b0d7191c361f5c6d63d5
Николай Бухалов <ni.bukhalov@gmail.com>
2024-10-09 22:02:19 +0300

    commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/crypto/acme/autocert"
+)
+
+func main() {
+	e := echo.New()
+	// e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("<DOMAIN>")
+	// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
+	e.AutoTLSManager.Cache = autocert.DirCache("/var/www/.cache")
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.GET("/", helloWorldHandler)
+	e.Logger.Fatal(e.StartAutoTLS(":443"))
+}
+
+func helloWorldHandler(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
